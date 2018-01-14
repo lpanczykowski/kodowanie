f666286b8989e61790bbf86837c7a9a5845ae8ca
Marcin Lewandowski <marclew484@student.polsl.pl>
2018-01-14 17:04:36 +0000

    x

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,33 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public struct User
+            {
+                public string Imie;
+                public byte wiek;
+            }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("El do Ka na wolno");
+            User [] Bar= new User[2];
+            Bar[0].Imie="Jan Kowalski";
+            Bar[0].wiek=32;
+            Bar[1].Imie="Andrzej Hhol";
+            Bar[1].wiek=54;
+
+            for(int i=0;i<Bar.Length;i++)
+                {
+                Console.WriteLine("{0,-15} | {1,5}", Bar[i].Imie, Bar[i].wiek);
+
+}
+            Console.ReadLine();
+
+           
+
+            /* string s=File.ReadAllText(@"C:\Users\Marcin\source\repos\kodowanie2\ConsoleApp1\ConsoleApp1\txt.txt");
+            Console.WriteLine(s);
             Console.ReadKey();
+            */
         }
     }
 }
